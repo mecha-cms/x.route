2fda61ef99570ab5b1e671be6a7664955824a88d
Taufik Nurrohman <t.nurrohman77@gmail.com>
2020-05-09 14:29:35 +0700

    LAUNCH!

---
+++ README.md
@@ -0,0 +1,9 @@
+Route Extension for Mecha
+=========================
+
+Release Notes
+-------------
+
+### 1.0.0
+
+ - Initial release.
---
+++ route/index.php
@@ -0,0 +1,13 @@
+<?php namespace _\lot\x;
+
+$r = \trim(\State::get('x.route.path') ?? 'route', '/');
+
+function route($id) {
+    if (\is_file($path = \LOT . \DS . 'route' . \DS . \strtr($id, '/', \DS) . '.php')) {
+        $GLOBALS['token'] = \Guard::token('route');
+        extract($GLOBALS, \EXTR_SKIP);
+        require $path;
+    }
+}
+
+\Route::set($r . '/*', __NAMESPACE__ . "\\route", 0);
