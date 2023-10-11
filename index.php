579120acf93a218108bba67a1bd91cce4a080ea1
Taufik Nurrohman <hi@taufik-nurrohman.com>
2023-10-11 09:23:24 +0700

    Bug Fix

--- index.php
+++ index.php
@@ -3,15 +3,15 @@
 function route($content, $path, $query, $hash) {
     foreach (\array_reverse(\array_values(\step(\trim(\strtr($path ?? "", ['/' => \D]), \D), \D))) as $v) {
         if (\is_file($file = \LOT . \D . 'route' . \D . $v . '.php')) {
-            if (\is_callable($fn = require $file)) {
+            if ($f = require $file) {
                 \status(200);
-                $content = \fire($fn, [$content, '/' . \strtr($v, [\D => '/']), $query, $hash]);
+                $content = \is_callable($f) ? \fire($f, [$content, $path, $query, $hash]) : $f;
             }
         }
     }
     return $content;
 }
 
-// This hook will be executed immediately, but the result may be overridden by later route hooks. The best way to
-// avoid this is to create route names that have never been used before.
+// This hook will fire immediately, but the result(s) returned may be overwritten by other `route` hook(s) that are
+// executed after it. The best way to avoid this effect is to create a route path that has never been used before.
 \Hook::set('route', __NAMESPACE__ . "\\route", 0);
